128a8f4353de1e613561475abb9c14b37a9a44e1
LaylaAlruwaisi <laylamarhoon80@gmail.com>
2025-04-29 23:11:35 +0300

    modified views, added user logic, worked on profile

--- Models/userDataSet.php
+++ Models/userDataSet.php
@@ -1,6 +1,83 @@
 <?php
 
+require ("Models/Database.php");
+require ("Models/userData.php");
+
 class userDataSet
 {
+    protected $_dbInstance, $_dbHandle;
 
+    public function __construct()
+    {
+        $this->_dbInstance = Database::getInstance();
+        $this->_dbHandle = $this->_dbInstance->getConnection();
+    }
+
+    public function addUserData($userID, $fname, $lname, $username, $password, $phoneNo, $regDate, $approved, $userRoleID)
+    {
+        if (strpos($username, "@admin.com") !== false)
+        {
+            $userRoleID = "1";
+        }
+        elseif (strpos($username, "@home.com") !== false)
+        {
+            $userRoleID = "2";
+        }
+        else
+        {
+            $userRoleID = "3";
+        }
+
+        //Hash password
+        $password = password_hash($password, PASSWORD_DEFAULT);
+
+        $sqlQuery = "INSERT INTO User (user_ID, f_Name, l_Name, username, password, Phone_no, Registration_date, Approved, User_role_User_role_ID) 
+                     VALUES (:userID, :fname, :lname, :username, :password, :phoneNo, :regDate, :approved, :userRoleID)";
+
+        $stmt = $this->_dbHandle->prepare($sqlQuery);
+
+        $stmt->bindParam(':userID', $userID);
+        $stmt->bindParam(':fname', $fname);
+        $stmt->bindParam(':lname', $lname);
+        $stmt->bindParam(':username', $username);
+        $stmt->bindParam(':password', $password);
+        $stmt->bindParam(':phoneNo', $phoneNo);
+        $stmt->bindParam(':regDate', $regDate);
+        $stmt->bindParam(':approved', $approved);
+        $stmt->bindParam(':userRoleID', $userRoleID);
+
+        $stmt->execute();
+    }
+
+    public function updateUserInfo($userID, $fname, $lname, $username, $phoneNo)
+    {
+        $sql = "UPDATE User 
+                SET f_Name = :fname,
+                    l_Name = :lname,
+                    username = :username,
+                    Phone_no = :phoneNo,
+                WHERE user_ID = :userID";
+
+        $stmt = $this->_dbHandle->prepare($sql);
+        $stmt->bindParam(':fname', $fname);
+        $stmt->bindParam(':lname', $lname);
+        $stmt->bindParam(':username', $username);
+        $stmt->bindParam(':phoneNo', $phoneNo);
+        $stmt->bindParam(':userID', $userID);
+
+        return $stmt->execute();
+    }
+
+    public function updateLocation($road, $city, $locationID)
+    {
+        $sql = "UPDATE Location 
+                SET city = :city, road = :road
+                WHERE location_ID = :locationID";
+
+        $stmt = $this->_dbHandle->prepare($sql);
+        $stmt->bindParam(':city', $city);
+        $stmt->bindParam(':road', $road);
+        $stmt->bindParam(':locationID', $locationID);
+        return $stmt->execute();
+    }
 }
--- Profile.php
+++ Profile.php
@@ -5,7 +5,31 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHP.php to edit this template
  */
 
+require ("Models/Database.php");
+require ("Models/UserDataSet.php");
+
 $view = new stdClass();
 
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    $address = array_map("trim", explode(",", $_POST["address"]));
+    $road = isset($address[0]) ? trim($address[0]) : '';
+    if (isset($address[1])) {
+        $road .= ", " . trim($address[1]);
+    }
+    $city = isset($address[2]) ? trim($address[2]) : '';
+
+    $userID = $_SESSION["userID"];
+    $fname = $_POST['fname'];
+    $lname = $_POST['lname'];
+    $username = $_POST['email'];
+    $phoneNo = $_POST['phno'];
+
+    $db = new UserdataSet();
+    $db->updateUserInfo($userID, $fname, $lname, $username, $phoneNo);
+    //$db->updateLocation($road, $city);
+
+    header("Location: Profile.php?userID=$userID&updated=true");
+}
 
 require_once("Views/Profile.phtml");
