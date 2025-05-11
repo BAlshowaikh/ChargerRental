6fda26896abe64d445e5d869a9894e3d3ce01d16
DaniaSuhail <202203819@student.polytechnic.bh>
2025-05-11 23:11:52 +0300

    made changes to ManageChargePoints

--- Models/chargerPointData.php
+++ Models/chargerPointData.php
@@ -1,34 +1,9 @@
 <?php
-// class ChargerPointData {
-//     private $homeownerId;
 
-//     public function __construct($homeownerId) {
-//         $this->homeownerId = $homeownerId;
-
-//         if (!isset($_SESSION['charge_points'])) {
-//             $_SESSION['charge_points'] = [];
-//         }
-//     }
-
-//     public function get() {
-//         $db = Database::getInstance()->getConnection();
-
-//         $stmt = $db->prepare("SELECT * FROM Charger_point WHERE User_user_ID = ?");
-//         $stmt->execute([$this->homeownerId]);
-//         return $stmt->fetch(PDO::FETCH_ASSOC);
-//     }
-
-
-//     public function save($data) {
-//         $_SESSION['charge_points'][$this->homeownerId] = $data;
-//     }
-
-class chargerPointData
-{
+class chargerPointData {
     private $id, $name, $description, $pricePerKW, $connectorType, $rating, $imageUrl, $availableStatusId, $userId, $locationId;
 
-    public function __construct($dbRow)
-    {
+    public function __construct(array $dbRow) {
         $this->id = $dbRow['Charger_point_ID'];
         $this->name = $dbRow['Name'];
         $this->description = $dbRow['Charger_point_description'];
@@ -41,15 +16,11 @@ class chargerPointData
         $this->locationId = $dbRow['Location_Location_ID'];
     }
 
-    public function getId() { return $this->id; }
-    public function getName() { return $this->name; }
-    public function getDescription() { return $this->description; }
-    public function getPricePerKW() { return $this->pricePerKW; }
-    public function getConnectorType() { return $this->connectorType; }
-    public function getRating() { return $this->rating; }
-    public function getImageUrl() { return $this->imageUrl; }
+    public function getName()              { return $this->name; }
+    public function getDescription()       { return $this->description; }
+    public function getPricePerKW()        { return $this->pricePerKW; }
+    public function getConnectorType()     { return $this->connectorType; }
+    public function getRating()            { return $this->rating; }
+    public function getImageUrl()          { return $this->imageUrl; }
     public function getAvailableStatusId() { return $this->availableStatusId; }
-    public function getUserId() { return $this->userId; }
-    public function getLocationId() { return $this->locationId; }
-
 }
